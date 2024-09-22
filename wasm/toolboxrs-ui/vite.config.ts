8e214716f0e78f688bbc6e9a1849b217132caf9d
Arne ten Ham <arnetenham@gmail.com>
2024-09-22 10:34:26 +0200

    add missing base url

--- wasm/toolboxrs-ui/vite.config.ts
+++ wasm/toolboxrs-ui/vite.config.ts
@@ -6,4 +6,5 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte(), wasm(), topLevelAwait()],
+    base: process.env.VITE_BASE_URL || '/', // Use base URL from env or default to '/'
 })
